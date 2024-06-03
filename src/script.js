1196922472a580bc6774c536e9122fe3095a64bb
emma <emma774922@gmail.com>
2024-06-04 00:13:21 +0800

    ver 1.0

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 //NavBar Toggle Class
 $(document).ready(function() {
-    var navMenu = $('.nav-menu-open')
+  var navMenu = $('.nav-menu-open')
   var navContainer = $('.nav-container')
+  var navItem = $('.nav-list li a')
 
     navMenu.click(function() {
         console.log('navMemu Click')
@@ -13,44 +14,30 @@ $(document).ready(function() {
     header.classList.toggle('sticky', window.scrollY>750)
   })
 
-  //   $(".fas").click(function () {
-  //     $(".fas").toggleClass("fa-bars fa-times");
-  // });
+  navItem.click(function() {
+    navContainer.removeClass('nav-show-menu');
+  });
 
-    // Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-    &&
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+
+
+  // Smooth scrolling for all links with hashes
+  $('a[href="#top"]').on('click', function(event) {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+        scrollTop: 0
+    }, 800);
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 750) {
+        $('.scrollTopBtn').fadeIn();
+    } else {
+        $('.scrollTopBtn').fadeOut();
     }
-  }
+  });
+
 });
-})
